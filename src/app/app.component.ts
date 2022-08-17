fb34651545b9805dfbd42ee0e8e1305f8ef10e85
Anug Saha <anugsaha@gmail.com>
2022-08-17 13:56:39 -0400

    start destination component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VacationHelper';
+  name = ""; 
+// budget variable
+  budget = 0 ; 
+// weather variable
+  weather = "any"; 
+
+  // List of Destinations with name, temperature and price range
+  destinations = [
+    { 
+      name: "Cancun",
+      budget: 1500, 
+      weather: "tropical"
+    }, 
+    { 
+      name: "Honolulu", 
+      budget: 2500, 
+      weather: "tropical"
+    }, 
+    { 
+      name: "Grand Canyon", 
+      budget: 800, 
+      weather: "hot"
+    }, 
+    { 
+      name: "Cairo", 
+      budget: 3000, 
+      weather: "hot"
+    }, 
+    { 
+      name: "Nuuk", 
+      budget: 1500, 
+      weather: "cold"
+    }, 
+    { 
+      name: "Denali National Park", 
+      budget: 1800, 
+      weather: "cold"
+    }, 
+  ]
+
+  showDestinations(){ 
+    //filter names of destinations and show user
+  }
+
 }
