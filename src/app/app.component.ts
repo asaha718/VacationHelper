ac257661a75db5d8488906e1866a7b2208996a9b
Youngyo Na <youngyona@gmail.com>
2022-08-17 15:31:26 -0400

    get vacation kind of done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { BoundTarget } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -14,7 +15,7 @@ export class AppComponent {
   weather = "any"; 
 
   // List of Destinations with name, temperature and price range
-  destinations = [
+  tropical_destinations = [
     { 
       name: "Cancun",
       budget: 1500, 
@@ -25,6 +26,8 @@ export class AppComponent {
       budget: 2500, 
       weather: "tropical"
     }, 
+  ]
+  hot_destinations = [
     { 
       name: "Grand Canyon", 
       budget: 800, 
@@ -35,6 +38,8 @@ export class AppComponent {
       budget: 3000, 
       weather: "hot"
     }, 
+  ]
+  cold_destinations = [
     { 
       name: "Nuuk", 
       budget: 1500, 
@@ -47,8 +52,42 @@ export class AppComponent {
     }, 
   ]
 
-  showDestinations(){ 
-    //filter names of destinations and show user
+  constructor() {
+    console.log("Iniating angular AppCompnent class");
   }
 
+
+  onSendMessage() {
+    this.name = this.calculateDestination(this.budget, this.weather);
+  }
+  calculateDestination(budget:number, weather:string){ 
+    let name = "Staycation";
+    switch(weather) {
+      case "tropical": {
+        for (let x=this.tropical_destinations.length-1; x>0; x--) {
+          if (this.tropical_destinations[x].budget <= budget) {
+            name = this.tropical_destinations[x].name;
+          }
+        }
+        break;
+      }
+      case "hot": {
+        for (let x=this.hot_destinations.length-1; x>0; x--) {
+          if (this.hot_destinations[x].budget <= budget) {
+            name = this.hot_destinations[x].name;
+          }
+        }
+        break;
+      }
+      case "cold": {
+        for (let x=this.cold_destinations.length-1; x>0; x--) {
+          if (this.cold_destinations[x].budget <= budget) {
+            name = this.cold_destinations[x].name;
+          }
+        }
+        break;
+      }
+    }
+    return name;
+  }
 }
